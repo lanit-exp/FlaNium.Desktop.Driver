e2e291728c6ac7d824a2658713ddacf7da24de4e
Uhanov Maxim <cmttwd@gmail.com>
2025-01-30 19:34:29 +0500

    CLIPBRD_E_CANT_OPEN error fix

--- FlaNium.Desktop.Driver/CommandExecutors/Auxiliary/GetClipboardTextExecutor.cs
+++ FlaNium.Desktop.Driver/CommandExecutors/Auxiliary/GetClipboardTextExecutor.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
 using FlaNium.Desktop.Driver.Common;
 
 namespace FlaNium.Desktop.Driver.CommandExecutors.Auxiliary {
@@ -7,13 +9,24 @@ namespace FlaNium.Desktop.Driver.CommandExecutors.Auxiliary {
 
         protected override JsonResponse  DoImpl() {
             string clipboardString = "";
+            const uint CLIPBRD_E_CANT_OPEN = 0x800401D0;
 
-            if (Clipboard.ContainsText()) {
-                clipboardString = Clipboard.GetText();
+            for (int i = 0; i < 50; i++) {
+                try {
+                    if (Clipboard.ContainsText()) {
+                        clipboardString = Clipboard.GetText();
+                    }
+                    return this.JsonResponse(ResponseStatus.Success, clipboardString);
+                }
+                catch (COMException e) {
+                    if ((uint)e.ErrorCode != CLIPBRD_E_CANT_OPEN) throw;
+                }
+                
+                Thread.Sleep(200);
             }
-
-
-            return this.JsonResponse(ResponseStatus.Success, clipboardString);
+            
+            return this.JsonResponse(ResponseStatus.UnknownError, "Module OpenClipboard failed (0x800401D0 (CLIPBRD_E_CANT_OPEN)). Iteration: 50, delay: 200.");
+            
         }
 
     }
